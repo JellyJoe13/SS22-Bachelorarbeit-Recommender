2e5c42fb9df02ead558ffdc66f45395304e8169f
Johannes Urban <a11911341@unet.univie.ac.at>
2022-04-10 22:56:02 +0200

    updated usages of function accuracy_precision_recall from accuracy_recommender.py. Added documentation and comments.

--- model_workspace/GNN_gcnconv_testspace.py
+++ model_workspace/GNN_gcnconv_testspace.py
@@ -27,17 +27,42 @@ class GNN_GCNConv_homogen(torch.nn.Module):
         self.bilinear = Bilinear(64, 64, 1)
         self.endflatten = Flatten(0, -1)
 
-    def fit_predict(self,
-                    x_input: torch.Tensor,
-                    edge_index_input: torch.Tensor,
-                    pos_edge_index_input: torch.Tensor
-                    ):
+    def fit_predict(
+            self,
+            x_input: torch.Tensor,
+            edge_index_input: torch.Tensor,
+            pos_edge_index_input: torch.Tensor
+    ) -> torch.Tensor:
+        """
+        Fit and predict function of the graph neural network predictor. Calculates a node embedding using the node data
+        and positive edges in the graph and then uses this embedding to calculate the logits.
+
+        Parameters
+        ----------
+        x_input : torch.Tensor
+            tensor containing the data of the nodes
+        edge_index_input : torch.Tensor
+            tensor containing the edges to predict
+        pos_edge_index_input : torch.Tensor
+            tensor containing the positive edges used for calculating the node embedding
+
+        Returns
+        -------
+        logits : torch.Tensor
+            predicted logits of the edges. will be put through sigmoid() function to get link probabilities
+        """
+        # input linear layer to learn the importance of the different chemical Descriptors or filter out the harmful
+        # ones
         x = self.init_linear(x_input)
-        x = self.conv1(x, pos_edge_index_input)  # first convolution layer
-        x = x.relu()  # relu function for tu - disables negative values
-        x = self.conv2(x, pos_edge_index_input)  # second convolution layer
-        # interpreting section
+        # first convolution layer - depth one
+        x = self.conv1(x, pos_edge_index_input)
+        # relu function for disabling negative values - GCNConv cannot handle them
+        x = x.relu()
+        # second convolution layer - depth two
+        x = self.conv2(x, pos_edge_index_input)
+        # interpreting section, similar to vector product but with weighting and possible bias. learnable parameters
         x = self.bilinear(x[edge_index_input[0]], x[edge_index_input[1]])
+        # return the logits
         return self.endflatten(x)
 
     @staticmethod
@@ -59,6 +84,10 @@ def get_link_labels(
         is_pos: bool,
         device_to_run
 ) -> torch.Tensor:
+    # deprecation warning
+    print("========================================================================")
+    print("Warning: This function is marked as deprecated and will be removed soon.")
+    print("========================================================================")
     if is_pos:
         return torch.ones(edge_index.size(1), device=device_to_run)
     else:
@@ -69,7 +98,24 @@ def train_model_batch(
         model: GNN_GCNConv_homogen,
         optimizer,
         data: torch_geometric.data.Data
-):
+) -> torch.Tensor:
+    """
+    Helper function that executes the train step for a batch data object.
+
+    Parameters
+    ----------
+    model : GNN_GCNConv_homogen
+        model to train on
+    optimizer
+        optimizer to optimize with
+    data : torch_geometric.data.Data
+        data used for the train process. shall contain x,y, edge_index and pos_edge_index
+
+    Returns
+    -------
+    loss : torch.Tensor
+        loss of the batch data object
+    """
     # set the model to train mode
     model.train()
     # set the optimizer
@@ -93,6 +139,22 @@ def test_model_batch(
         model: GNN_GCNConv_homogen,
         data: torch_geometric.data.Data
 ):
+    """
+    Helper function that executes a simple evaluation for a batch data object and determines the logits of the edges to
+    predict.
+
+    Parameters
+    ----------
+    model : GNN_GCNConv_homogen
+        model to run the test on
+    data : torch_geometric.data.Data
+        data with contains the edges, pos_edges and node data to run on
+
+    Returns
+    -------
+    link_logits : torch.Tensor
+        tensor containing the approximations of the edges
+    """
     # put the model in evaluation mode
     model.eval()
     # call fit_predict though it will not be fitted due to the @torch.no_grad() annotation
@@ -103,19 +165,43 @@ def test_model_batch(
 def test_model_advanced(
         model: GNN_GCNConv_homogen,
         batcher: edge_batch.EdgeConvolutionBatcher,
-        id_breakpoint: int,
         epoch: int = 0
-) -> tuple(int, int):
+) -> tuple(float, float):
+    """
+    Execute full test creating a roc curve diagram and calculating the accuracy scores.
+
+    Parameters
+    ----------
+    model : GNN_GCNConv_homogen
+        model on which to test
+    batcher : edge_batch.EdgeConvolutionBatcher
+        batcher from which to fetch the batch data objects from
+    epoch : int
+        Additional information that is used for naming the saved plot of the roc curve.
+
+    Returns
+    -------
+    precision, recall : tuple(float, float)
+        precision and recall score of the whole test procedure spanning all batches
+    """
+    # poll first element from batcher stack
     current_batch, retranslation_dict = batcher.next_element()
+    # create empty lists to append the results to in the while loop
     batch_loop_storage = []
     edge_index_transformed = []
+    # for all batch data objects in batcher do
     while current_batch:
-        link_logits = test_model_basic(model, current_batch)
-        batch_loop_storage.appen((current_batch.y,
+        # execute test model batch which gets the logits of the edges
+        link_logits = test_model_batch(model, current_batch)
+        # append labels and logits to storage list
+        batch_loop_storage.append((current_batch.y,
                                   link_logits))
+        # poll next element
         current_batch, retranslation_dict = batcher.next_element()
+        # write and transform edge information to new edge_index (in list form)
         for edge in current_batch.edge_index.T:
             edge_index_transformed.append([retranslation_dict[edge[0]], retranslation_dict[edge[1]]])
+    # fuze batch results
     link_logits = torch.cat([i[0] for i in batch_loop_storage])
     link_labels = torch.cat([i[1] for i in batch_loop_storage])
     edge_index_transformed = torch.tensor(edge_index_transformed, dtype=torch.long).T
@@ -129,8 +215,7 @@ def test_model_advanced(
     # calculate accuracy
     precision, recall = accuracy_recommender.accuracy_precision_recall(edge_index_transformed,
                                                                        link_labels,
-                                                                       link_logits,
-                                                                       id_breakpoint)
+                                                                       link_logits)
     return precision, recall
 
 
@@ -162,7 +247,7 @@ def test_model_basic(
     logits_list = []
     # for all batch objects in the batcher do
     while current_batch:
-        # execute the test for the the batches and append their logits and labels to the storage list
+        # execute the test for the batches and append their logits and labels to the storage list
         logits_list.append((test_model_basic(model, current_batch), current_batch.y))
         # poll the next element from the stack
         current_batch = batcher.next_element()
