6e1bac91c6094cbf21737c6837dc3b9c46deb9c1
Johannes Urban <a11911341@unet.univie.ac.at>
2022-04-16 21:36:24 +0200

    added the file for GIN Conv, but not sure if used because it requires torch.nn.Module to work and not sure how this works

---
+++ model_workspace/GNN_minibatch_homogen_GINConv.py
@@ -0,0 +1,9 @@
+import torch.nn
+
+
+# todo: ... basically understand what it does and why it wants a torch.nn.Module parameter?... or just let it be
+class GNN_GINConv(torch.nn.Module):
+    def __init__(
+            self
+    ):
+        return None
